3348b92a7912d9b0205880a07cfba1404d6eebe3
Andrzej Sobczyk <nooneeverreallydies@tlen.pl>
2020-08-06 20:02:15 +0200

    new question started

--- src/components/NewQuestion.js
+++ src/components/NewQuestion.js
@@ -2,10 +2,20 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
 class NewQuestion extends Component {
+  handleSubmit = ()=> {
+
+  }
+
   render() {
     return (
       <div>
-        NEW QUESTION
+        <p>Would you rather:</p>
+        <form onSubmit={ this.handleSubmit() }>
+          <input></input>
+          <p>OR</p>
+          <input></input>
+          <button>Submit</button>
+        </form>
       </div>
     )
   }
