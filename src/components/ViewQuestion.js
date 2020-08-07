b73fa136c5da036967dacfdab90b98b98870b2a1
Andrzej Sobczyk <nooneeverreallydies@tlen.pl>
2020-08-07 09:58:49 +0200

    viewQuestion works

--- src/components/ViewQuestion.js
+++ src/components/ViewQuestion.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { setAuthedUser } from '../actions/authedUser'
 
 class ViewQuestion extends Component {
   render() {
-    const { question, users, authedUser } = this.props
-    console.log(this.props)
+    const { users, authedUser, questions } = this.props
+    const id = this.props.match.params.id
+    const answered = !!users[authedUser].answers[id]
+    const question = questions[id]
 
     return (
       <div>
@@ -12,14 +15,14 @@ class ViewQuestion extends Component {
         <div>
           <img src={ users[authedUser].avatarURL } alt='avatar' width='100' height='100' />
           <p>Would you rather:</p>
-          { (this.props.answered === true) 
+          { (answered === true) 
             ? (<div>
               <p>{ question.optionOne.text }{ question.optionOne.votes.some((vote)=> vote === authedUser) === true && ' (your vote)' }</p>
               <p>{ question.optionOne.votes.length * 100 / (question.optionOne.votes.length + question.optionTwo.votes.length) } %
-                ({ question.optionOne.votes.length } vote{ question.optionOne.votes.length > 1 && 's' })</p>
+                ({ question.optionOne.votes.length } vote{ (question.optionOne.votes.length > 1 || question.optionOne.votes.length === 0) && 's' } out of { question.optionOne.votes.length + question.optionTwo.votes.length })</p>
               <p>{ question.optionTwo.text }{ question.optionTwo.votes.some((vote)=> vote === authedUser) === true && ' (your vote)' }</p>
               <p>{ question.optionTwo.votes.length * 100 / (question.optionOne.votes.length + question.optionTwo.votes.length) } %
-                ({ question.optionTwo.votes.length } vote{ question.optionTwo.votes.length > 1 && 's' })</p>
+                ({ question.optionTwo.votes.length } vote{ (question.optionTwo.votes.length > 1 || question.optionTwo.votes.length === 0) && 's' } out of { question.optionOne.votes.length + question.optionTwo.votes.length })</p>
             </div>)
             : (<div>
               <p>{ question.optionOne.text }</p>
@@ -33,14 +36,11 @@ class ViewQuestion extends Component {
   }
 }
 
-function mapStateToProps({ authedUser, questions, users }, { id, answered }) {
-  const question = questions[id]
-
+function mapStateToProps({ authedUser, questions, users }) {
   return {
     authedUser,
-    question,
-    users,
-    answered
+    questions,
+    users
   }
 }
 
